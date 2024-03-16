413629cd3d483ce40be2eab4de84e566f56635b5
QuitoTactico <teteban0917@gmail.com>
2024-03-15 20:18:12 -0500

    Gráfica rudimentaria

--- cap_1/views.py
+++ cap_1/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .functions.biseccion import biseccion_func
 # Create your views here.
 
+import io 
+import urllib, base64
+
 def home(request):
     return render(request, 'index.html')
 
@@ -18,10 +21,15 @@ def test(request):
 
         response = biseccion_func(funcion, a, b, tol, niter)
 
+        img = response['img']
+        #img = io.BytesIO(urllib.parse.unquote(img).encode('utf-8'))
+        #img = base64.b64encode(img.getvalue()).decode()
+
+
         return render(request, 'test.html', {'sol'  : response['sol'], 
                                              'iter' : response['iter'],
                                              'tabla': response['tabla'],
-                                             'img'  : response['img'],
+                                             'img'  : img,
                                              'final': response['final']})
     else:
         return render(request, 'test.html')
