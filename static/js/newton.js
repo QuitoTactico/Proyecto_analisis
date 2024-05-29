b675c9ded619a1c7f70e0909bab19620f6787214
QuitoTactico <teteban0917@gmail.com>
2024-05-29 04:24:11 -0500

    newton arreglado

--- static/js/newton.js
+++ static/js/newton.js
@@ -4,70 +4,40 @@ window.onload = function () {
     let iter = document.getElementById('iter');
     let mensaje = document.getElementById('mensaje');
 
-    //let resultsTable = document.getElementById('resultsTable');
-    //let tab_soluciones = document.getElementById('soluciones');
     let tab_soluciones = document.getElementsByClassName('soluciones');
     let resultsTable = document.getElementsByClassName('resultsTable');
-    //let tablaprueba1 = document.getElementById('tablaprueba1');
 
     let formData = JSON.parse(sessionStorage.getItem('formData'));
     if (formData) {
-            document.getElementById('funcion').value = formData.funcion;
-            document.getElementById('x0').value = formData.x0;
-            document.getElementById('a').value = formData.a;
-            document.getElementById('b').value = formData.b;
-            document.getElementById('tol').value = formData.tol;
-            document.getElementById('niter').value = formData.iter;
-            resultsTable[0].style.display = 'table'; // show the table
-            solucion.style.display = 'block';
-            iter.style.display = 'block';
-            mensaje.style.display = 'block';
-            tab_soluciones[0].style.display = 'table';
+        document.getElementById('funcion').value = formData.funcion;
+        document.getElementById('x0').value = formData.x0;
+        document.getElementById('tol').value = formData.tol;
+        document.getElementById('niter').value = formData.niter;
+
+        // Show the table if form data is present in session storage
+        for (let i = 0; i < resultsTable.length; i++) {
+            resultsTable[i].style.display = 'table';
+        }
+        solucion.style.display = 'block';
+        iter.style.display = 'block';
+        mensaje.style.display = 'block';
+        tab_soluciones[0].style.display = 'table';
     }
-    else {
-        sessionStorage.clear();
-        resultsTable[0].style.display = 'none';
-        solucion.style.display = 'none';
-        iter.style.display = 'none';
-        mensaje.style.display = 'none';
-        tab_soluciones[0].style.display = 'none';
-    }
+    console.log(resultsTable);
+    console.log(tab_soluciones);
+    console.log(formData);
 
     form.addEventListener('submit', function (e) {
+        sessionStorage.clear();
         let funcion = document.getElementById('funcion').value;
         let x0 = document.getElementById('x0').value;
-        let a = document.getElementById('a').value;
-        let b = document.getElementById('b').value;
         let tol = document.getElementById('tol').value;
-        let iter = document.getElementById('niter').value;
-
-            sessionStorage.setItem('formData', JSON.stringify({
-                funcion: funcion,
-                x0:x0,
-                a: a,
-                b: b,
-                tol: tol,
-                iter: iter,
-
-            }));
-        // If form fields are not empty, show the table
-        if (funcion !== '' && a !== '' && b !== '' && tol !== '' && iter !== '') {
-            resultsTable.style.display = 'table'; // show the table
-        }
-
+        let niter = document.getElementById('niter').value;
+        sessionStorage.setItem('formData', JSON.stringify({
+            funcion: funcion,
+            x0: x0,
+            tol: tol,
+            niter: niter,
+        }));
     });
-
-    if (!formData) {
-        document.getElementById('funcion').value = '';
-        document.getElementById('a').value = '';
-        document.getElementById('b').value = '';
-        document.getElementById('tol').value = '';
-        document.getElementById('niter').value = '';
-        resultsTable[0].style.display = 'none';
-        solucion.style.display = 'none';
-        iter.style.display = 'none';
-        mensaje.style.display = 'none';
-        tab_soluciones[0].display = 'none';
-        sessionStorage.clear();
-    }
 }
